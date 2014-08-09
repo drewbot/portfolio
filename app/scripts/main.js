96ffe8e7e02de7376e9c3859ef49432f521b7662
Drew Botka <hellodrewbot@gmail.com>
2014-08-09 17:45:33 -0400

    removing ember from this project

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,17 +1,1 @@
 
-
-App = Ember.Application.create({
-	// ready: function() {
- //    this.register('main:auth', App.AuthController);
- //    this.inject('route', 'auth', 'main:auth');
- //    this.inject('controller', 'auth', 'main:auth');
- //  }
-});
-
-// App Adapter 
-
-App.ApplicationAdapter = DS.FirebaseAdapter.extend({
-    firebase: new Firebase("https://portfoliodrewbotka.firebaseio.com/")
-  });
-
-App.ApplicationSerializer = DS.FirebaseSerializer.extend();
