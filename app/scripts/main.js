c0dde1538b1d499e75e33defe25f72e8a1e1dc84
Drew Botka <hellodrewbot@gmail.com>
2015-02-13 11:16:58 -0500

    added functioning responsive css triangles to work and contact

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,78 +1,32 @@
-// When document loads run main section animations
-$( document ).ready(function() {
-    $('.main-copy-headline').addClass('main-copy-headline-bounce');
-    $('.main-copy-paragraph').addClass('main-copy-paragraph-bounce');
-    $('.logo').addClass('logo-bounce');
+var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0)
+console.log(w);
+$(".work-triangle").css({
+  "border-top": w / 10 + 'px solid #01A0B0'
+});
+$(".work-triangle").css({
+  "border-right": w + 'px solid transparent'
+});
+$(".contact-triangle").css({
+  "border-top": w / 10 + 'px solid #2A363B'
+});
+$(".contact-triangle").css({
+  "border-left": w + 'px solid transparent'
 });
 
-// Home button
-$('.home-button').click(function(){
-	$('.main-copy-headline').addClass('main-copy-headline-bounce');
-    $('.main-copy-paragraph').addClass('main-copy-paragraph-bounce');
-    $('.logo').addClass('logo-bounce');
-    $('.main-copy-headline').removeClass('main-copy-headline-bounce');
-    $('.main-copy-paragraph').removeClass('main-copy-paragraph-bounce');
-    console.log('click worked')
-})
-
-$('.to-top').click(function(){
-	jQuery('html,body').animate({scrollTop:0},0);
-})
-
-
-
-
-////////////////////////////////////////////////// Way points
-
-////////////////////////////////////////////////// Main div
-
-////////////////////////////////////////////////// Second div
-////////////////////////////////////////// transitions related to Main div triggered by second
-$('.second').waypoint(function(direction) {
-	if (direction === 'down') {
-		$('.main-copy-headline').removeClass('main-copy-headline-bounce');
-    	$('.main-copy-paragraph').removeClass('main-copy-paragraph-bounce');
-	} 
-}, { offset: '0%' })
-
-$('.second').waypoint(function(direction) {
-	if (direction === 'up') {
-		$('.main-copy-headline').addClass('main-copy-headline-bounce');
-    	$('.main-copy-paragraph').addClass('main-copy-paragraph-bounce');
-	} 
-}, { offset: '100%' })
-
-
-$('.second').waypoint(function(direction) {
-	if (direction === 'down') {
-		$('.box-two').addClass('box-two-move');
-	} else {
-		$('.box-two').removeClass('box-two-move');
-	}
-}, { offset: '50%' });
-
-$('.second').waypoint(function(direction) {
-	if (direction === 'down') {
-		$('.box-two').removeClass('box-two-move');
-	} else {
-		$('.box-two').addClass('box-two-move');
-	}
-}, { offset: '0%' });
-
-
-////////////////////////////////////////////////// Third div
-$('.third').waypoint(function(direction) {
-	if (direction === 'down') {
-		$('.box-three').addClass('box-two-move');
-	} else {
-		$('.box-three').removeClass('box-two-move');
-	}
-}, { offset: '50%' });
-
-$('.third').waypoint(function(direction) {
-	if (direction === 'down') {
-		$('.box-three').removeClass('box-two-move');
-	} else {
-		$('.box-three').addClass('box-two-move');
-	}
-}, { offset: '0%' });
+window.onresize = function(event) {
+  var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0)
+	console.log(w);
+	// $('.work-triangle').css( "border-width-left" , w )
+	$(".work-triangle").css({
+    "border-top": w / 10 + 'px solid #01A0B0'
+  });
+  $(".work-triangle").css({
+    "border-right": w + 'px solid transparent'
+  });
+  $(".contact-triangle").css({
+	  "border-top": w / 10 + 'px solid #2A363B'
+	});
+	$(".contact-triangle").css({
+	  "border-left": w + 'px solid transparent'
+	});
+};
