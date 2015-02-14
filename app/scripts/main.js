2a00af9145393d1bcf36df453151a5ea2cf15380
Drew Botka <hellodrewbot@gmail.com>
2015-02-13 22:57:29 -0500

    set up basic gallery styling

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,7 +2,6 @@
 
 var getWindowSetTriangle = function(){
 	var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0)
-	console.log(w);
 	// Set css triangles on work section and contact section to window width
 	$(".work-triangle").css({
 	  "border-top": w / 10 + 'px solid #01A0B0'
@@ -18,11 +17,20 @@ var getWindowSetTriangle = function(){
 	});
 }
 
+var getWidthSetHeight = function(){
+	var imageWidth = $('.work-content div').width();
+	$('.work-content div').height(imageWidth / 1.3);
+	$('.work-content div a').width(imageWidth);
+	$('.work-content div a').height(imageWidth / 1.3);
+}
+
 getWindowSetTriangle();
+getWidthSetHeight();
 
 // On resize get window width
 window.onresize = function(event) {
   getWindowSetTriangle();
+  getWidthSetHeight();
 };
 
 
@@ -31,25 +39,22 @@ window.onresize = function(event) {
 
 // focus in on new item input changes border color
 $('.contact-name input').focusin( function(){
-	console.log("focus in name")
 	$(this).css({
-		"border-width" : "0 0 2px 0"
+		"border-bottom-color" : "#D66611"
 	});
 	$(this).attr('placeholder','');
 	$('.name-label').toggleClass('input-focus-animate');
 });
 $('.contact-email input').focusin( function(){
-	console.log("focus in email")
 	$(this).css({
-		"border-width" : "0 0 2px 0"
+		"border-bottom-color" : "#D66611"
 	});
 	$(this).attr('placeholder','');
 	$('.email-label').toggleClass('input-focus-animate');
 });
 $('.contact-message textarea').focusin( function(){
-	console.log("focus in message")
 	$(this).css({
-		"border-width" : "0 0 2px 0"
+		"border-bottom-color" : "#D66611"
 	});
 	$(this).attr('placeholder','');
 	$('.message-label').toggleClass('input-focus-animate');
@@ -57,25 +62,22 @@ $('.contact-message textarea').focusin( function(){
 
 // focus out on new item input changes border color back
 $('.contact-name input').focusout( function(){
-	console.log("focus out name")
 	$(this).css({
-		"border-width" : "0 0 1px 0"
+		"border-bottom-color" : "#ffffff"
 	});
 	$(this).attr('placeholder','What\'s your name?');
 	$('.name-label').toggleClass('input-focus-animate');
 });
 $('.contact-email input').focusout( function(){
-	console.log("focus out email")
 	$(this).css({
-		"border-width" : "0 0 1px 0"
+		"border-bottom-color" : "#ffffff"
 	});
 	$(this).attr('placeholder','...your email?');
 	$('.email-label').toggleClass('input-focus-animate');
 });
 $('.contact-message textarea').focusout( function(){
-	console.log("focus out message")
 	$(this).css({
-		"border-width" : "0 0 1px 0"
+		"border-bottom-color" : "#ffffff"
 	});
 	$(this).attr('placeholder','What\'s up?');
 	$('.message-label').toggleClass('input-focus-animate');
